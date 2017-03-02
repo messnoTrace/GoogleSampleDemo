b38a1f2e7f57c43110ddf37ea30dd2da03cde7d2
messnoTrace <cy_nforget@126.com>
2017-03-02 14:20:45 +0800

    add activity transition

--- GooleSampleDemo/app/src/main/java/com/notrace/MainActivity.java
+++ GooleSampleDemo/app/src/main/java/com/notrace/MainActivity.java
@@ -1,13 +1,23 @@
 package com.notrace;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.notrace.activitytransition.TransitionActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void activityTransition(View view)
+    {
+        startActivity(new Intent(this, TransitionActivity.class));
     }
 }
